1bab42bdea72d9d4237b36b8133cf4eb5ba8cc59
lcorsini <lcorsini@gmail.com>
2015-05-28 13:12:14 +0400

    Removed unnecessary event body repetition Added current year instead of hardcoding (yes it's embarassing) Prettier console output

--- turni.py
+++ turni.py
@@ -2,6 +2,8 @@ import gspread
 import httplib2
 import time
 import locale
+import pprint
+from datetime import datetime
 from collections import OrderedDict
 from apiclient.discovery import build
 from oauth2client.client import Credentials
@@ -25,7 +27,7 @@ credentials = __create_sheets_service()
 gc = gspread.authorize(credentials)
 
 # Open a worksheet from spreadsheet with one shot
-wks = gc.open("Turni operation 2014").sheet1
+wks = gc.open("Turni operation 2015").sheet1
 month = wks.title
 if any ( [month == 'Aprile', month=='Giugno', month=='Settembre', month=='Novembre'] ):
 	turn_list = wks.range('E2:E31')
@@ -37,10 +39,11 @@ else:
 	turn_list = wks.range('E2:E32')
 	days_list = wks.range('A2:A32')
 	
+
 days = []
 for member in days_list:
      #print member.value
-     date_string=member.value+" "+month+" "+"2014"
+     date_string=member.value+" "+month+" "+str(datetime.now().year)
      locale.setlocale(locale.LC_TIME, 'it_IT')
      date_obj = time.strptime(date_string, '%a %d %B %Y')
      locale.setlocale(locale.LC_TIME, 'en_US')
@@ -53,32 +56,42 @@ for member in turn_list:
     turns.append(member.value)
 
 dictionary = OrderedDict(zip(days, turns))
-#print dictionary
-
-
+pprint.pprint(list(dictionary.items()))
 ## Calendar Section
 service = __create_cal_service()
-calendar='CALENDAR_ID'
+calendar = 'CALENDAR_ID'
 oldEvents = service.events().list(calendarId=calendar).execute()
 for event in oldEvents['items']:
-    print event['id'] + ' ' + event['summary'] + ' ' + 'deleted'
-    service.events().delete(calendarId=calendar, eventId=event['id']).execute()
+	print 'Event: ' + event['summary'] + '\tEventID: '+event['id'] + '\tdeleted' 
+	service.events().delete(calendarId=calendar, eventId=event['id']).execute()
 for key in dictionary:
 	print key , 'corresponds to', dictionary[key]
 	if dictionary[key] == '1':
 		startTime = '07:00:00'
 		endTime = '16:00:00'
-		event = {'summary' : 'Turno '+dictionary[key],'end' : {'timeZone' : 'Europe/Rome', 'dateTime' : key+'T'+endTime }, 'start' : { 'timeZone' : 'Europe/Rome', 'dateTime' : key+'T'+startTime }}
 	elif dictionary[key] == '2':
 		startTime = '09:00:00'
 		endTime = '18:00:00'
-		event = {'summary' : 'Turno '+dictionary[key],'end' : {'timeZone' : 'Europe/Rome', 'dateTime' : key+'T'+endTime }, 'start' : { 'timeZone' : 'Europe/Rome', 'dateTime' : key+'T'+startTime }}
 	elif dictionary[key] == '3':
 		startTime = '10:30:00'
 		endTime = '19:30:00'
-		event = {'summary' : 'Turno '+dictionary[key],'end' : {'timeZone' : 'Europe/Rome', 'dateTime' : key+"T"+endTime }, 'start' : { 'timeZone' : 'Europe/Rome', 'dateTime' : key+'T'+startTime }}
+	elif dictionary[key] == '4':
+		startTime = '08:00:00'
+		endTime = '12:00:00'
 	else:
 		continue
-	request = service.events().insert(calendarId=calendar, body=event)
-	response = request.execute()
-	print response
+	event = {'summary' : 'Turno '+dictionary[key],
+			 'description' : '#Turno'+dictionary[key],
+			 'end' : {
+			 	 'timeZone' : 'Europe/Rome', 
+			 	 'dateTime' : key+'T'+endTime 
+			 	 }, 
+			 'start' : { 
+			 	 'timeZone' : 'Europe/Rome', 
+			 	 'dateTime' : key+'T'+startTime 
+			 	 }
+			 }
+
+	request = service.events().insert(calendarId=calendar, body=event).execute()
+	#response = request.execute()
+	#print request
